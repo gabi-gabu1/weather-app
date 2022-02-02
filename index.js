ef1743cbaf349a4addbe4aa82056552f17372be2
gabi-gabu1 <97314479+gabi-gabu1@users.noreply.github.com>
2022-02-02 16:16:27 +0100

    Adding weather forecast using API

--- index.js
+++ index.js
@@ -25,6 +25,61 @@ let minutes = addZero(now.getMinutes());
 
 time.innerHTML = `${day} ${hours}:${minutes}`;
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
+// forecast
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+  <div class="col-2">
+    <div class="forecast-day">${formatDay(forecastDay.dt)}</div>
+
+    <img src="http://openweathermap.org/img/wn/${
+      forecastDay.weather[0].icon
+    }@2x.png" 
+    alt=""
+    width="42"/>
+    <div class="forecast-temperature">
+      <span class="forecast-temperature-max"> ${Math.round(
+        forecastDay.temp.max
+      )}° </span>
+      <span class="forecast-temperature-min"> ${Math.round(
+        forecastDay.temp.min
+      )}° </span>
+  </div>
+  </div>
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+///
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "a592e749eb4eda83dffcd2b9176f3c7f";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function getTemperature(response) {
   let cityTemperature = Math.round(response.data.main.temp);
   let celsius = document.querySelector("#temperature");
@@ -48,6 +103,8 @@ function getTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].icon);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
